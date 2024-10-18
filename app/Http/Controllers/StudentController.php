698071af4f8ac51bd7da52f229af31ad0f9e02af
isurubandara1 <isurumbandara@gmail.com>
2024-10-18 16:52:01 +0530

    get function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
-    //
+    Public function index(){
+        $students = Student::all();
+
+       // return response()->json($students);
+
+       $data = [
+           'status' => 200,
+           'students' => $students,
+       ];
+       return response()->json($data,200,
+       );
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route:: get('one',function(){
     return 'one';
 });
+
+Route::get('students', [StudentController::class, 'index']);
