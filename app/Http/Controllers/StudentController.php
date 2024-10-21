8e9e15168003f27fedf4ab9fe0387e0a16cfca13
isurubandara1 <isurumbandara@gmail.com>
2024-10-21 11:49:45 +0530

    put method add

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -19,4 +20,36 @@ class StudentController extends Controller
        return response()->json($data,200,
        );
     }
+
+    Public function upload(Request $request){
+        $validator= Validator::make($request->all(),
+        ['name' => 'required',
+        'email' => 'required|email']);
+
+        if($validator->fails()){
+
+            $data = [
+                'status' => 422,
+                'message' => $validator->messages(),
+            ];
+
+            return response()->json($data, 422);
+        }
+        else{
+            $student = new Student();
+            $student->name = $request->name;
+            $student->email = $request->email;
+            $student->save();
+
+            $data = [
+                'status' => 200,
+                'message' => 'Student added successfully',
+            ];
+
+            return response()->json($data, 200);
+        }
+    
+    }
+
+    
 }
